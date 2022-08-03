fc4e845fa897c700d16d345b77f6e34378058b8e
SheldonWilton <sheldon.wilton@technosoftja.com>
2022-08-02 21:22:46 -0500

    feat(setup): created layout of the app and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import "./styles/app.css";
+
 function App() {
   return (
-    <div className="">
-      <header className="">
-      </header>
+    <div className="app_layout">
+      <div className="app_sidebar">
+      </div>
+
+      <div className="app_content_wrapper">
+        
+        <div className="app_header">
+          <h2>
+            Hotel Management System
+          </h2>
+        </div>
+
+        <div className="app_content">
+          
+        </div>
+      </div>      
     </div>
   );
 }
