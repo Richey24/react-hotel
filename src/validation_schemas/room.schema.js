811b7f7e3106b8cdc4c97df8995aaecc641e4c50
SheldonWilton <sheldon.wilton@technosoftja.com>
2022-08-11 21:42:26 -0500

    feat(room): setup redux and fetching data from the api

--- src/validation_schemas/room.schema.js
+++ src/validation_schemas/room.schema.js
@@ -1,12 +1,12 @@
 import * as yup from "yup";
 
 const roomSchema = yup.object().shape({
-  number: yup
+  roomNum: yup
     .number()
     .typeError("Value must be of type number")
     .positive("Number can't be below zero (0)")
     .required("Required"),
-  type: yup.string().required("Required"),
+  category: yup.string().required("Required"),
   price: yup
     .number()
     .positive("Price can't be below zero (0)")
