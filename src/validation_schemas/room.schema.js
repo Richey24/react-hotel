6d2aaca8deb39853a1819e6649ee845a10da2b34
SheldonWilton <sheldon.wilton@technosoftja.com>
2022-08-11 22:23:03 -0500

    feat(room): commented the schema

--- src/validation_schemas/room.schema.js
+++ src/validation_schemas/room.schema.js
@@ -1,16 +1,25 @@
 import * as yup from "yup";
 
+/**
+ * This schema is used to validate the form on the
+ * room page each function describes what the 
+ * form will validate for.
+ */ 
 const roomSchema = yup.object().shape({
   roomNum: yup
-    .number()
+    .number() //checks if the room number is a type of number
     .typeError("Value must be of type number")
-    .positive("Number can't be below zero (0)")
-    .required("Required"),
-  category: yup.string().required("Required"),
+    .positive("Number can't be below zero (0)") //checks if the room number is a positive value
+    .required("Required"), //checks if the user entered room number
+
+  category: yup
+  .string() //checks if room category is a string
+  .required("Required"),//checks if the user entered a category
+  
   price: yup
-    .number()
-    .positive("Price can't be below zero (0)")
-    .required("Required"),
+    .number()//checks if the price value is a type of number
+    .positive("Price can't be below zero (0)")//checks if the price is a positive value
+    .required("Required"),//checks if the user entered a price
 });
 
 export default roomSchema;
