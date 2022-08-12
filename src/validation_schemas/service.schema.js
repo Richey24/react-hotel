6412179c921be8a2bf1394f9b1f725f22033435b
SheldonWilton <sheldon.wilton@technosoftja.com>
2022-08-11 22:19:55 -0500

    feat(service): added comments to schema

--- src/validation_schemas/service.schema.js
+++ src/validation_schemas/service.schema.js
@@ -1,9 +1,22 @@
 import * as yup from "yup"
 
+/**
+ * This schema is used to validate the form on the
+ * customers page each function describes what the 
+ * form will validate for.
+ */ 
 const serviceSchema = yup.object().shape({
-  roomNum: yup.number().required("Required"),
-  description: yup.string().required("Required"),
-  date: yup.date().required("Required"),
+  roomNum: yup
+  .number() // checks if the service is a type of number
+  .required("Required"), //checks if the user entered a room value
+  
+  description: yup
+  .string() //checks if the service type is a type of string
+  .required("Required"), //checks if the user entered a value for the service type
+  
+  date: yup
+  .date() //checks if the service assignDate is a type of date
+  .required("Required"), //checks if the user entered a assigned date value
 });
 
 export default serviceSchema;
